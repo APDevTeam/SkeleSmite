6936dd0bd0002ac6c2589456e572563ccaa2061d
WhiteWolfdoge <emjwhite@outlook.com>
2017-02-20 21:08:31 -0800

    Plugin base

---
+++ src/net/whitewolfdoge/skelesmite/SkSmPlugin.java
@@ -0,0 +1,21 @@
+package net.whitewolfdoge.skelesmite;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SkSmPlugin extends JavaPlugin{
+	
+	@Override
+	public void onLoad(){
+		// Do nothing
+	}
+	
+	@Override
+	public void onEnable(){
+		// Do nothing
+	}
+	
+	@Override
+	public void onDisable(){
+		// Do nothing
+	}
+}
