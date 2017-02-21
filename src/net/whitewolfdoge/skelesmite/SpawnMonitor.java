c8d134211ae69ccc0e5c12b84b0d922eeba6b5a8
cccm5 <ccccm5@gmail.com>
2017-02-21 09:20:05 -0500

    darn git

--- src/net/whitewolfdoge/skelesmite/SpawnMonitor.java
+++ src/net/whitewolfdoge/skelesmite/SpawnMonitor.java
@@ -8,6 +8,7 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 public class SpawnMonitor implements Listener{
 	
 	@EventHandler
+	public void onCreatureSpawnEvent(CreatureSpawnEvent ee){
 		if(ee.getEntity() instanceof Horse && ((Horse)ee.getEntity()).getVariant().equals(Horse.Variant.SKELETON_HORSE))
 			ee.setCancelled(true);
 	}
