6bda6a34913365a2692c6ba7315404377dd3e753
WhiteWolfdoge <emjwhite@outlook.com>
2017-02-20 22:26:33 -0800

    Register and unregister listeners

--- src/net/whitewolfdoge/skelesmite/SkSmPlugin.java
+++ src/net/whitewolfdoge/skelesmite/SkSmPlugin.java
@@ -1,21 +1,30 @@
 package net.whitewolfdoge.skelesmite;
 
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SkSmPlugin extends JavaPlugin{
+	Listener spawnMon, chkMon;
 	
 	@Override
 	public void onLoad(){
-		// Do nothing
+		spawnMon = new SpawnMonitor();
+		chkMon = new ChunkLoadMonitor();
 	}
 	
 	@Override
 	public void onEnable(){
-		// Do nothing
+		getServer().getPluginManager().registerEvents(spawnMon, this);
 	}
 	
 	@Override
 	public void onDisable(){
-		// Do nothing
+		EntitySpawnEvent.getHandlerList().unregister(spawnMon);
+		spawnMon = null;
+		
+		ChunkLoadEvent.getHandlerList().unregister(chkMon);
+		chkMon = null;
 	}
 }
