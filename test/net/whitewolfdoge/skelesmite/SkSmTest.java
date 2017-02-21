bda022b70867c61eeffeeafe8abab48c613e4b9c
WhiteWolfdoge <emjwhite@outlook.com>
2017-02-20 22:26:43 -0800

    string format test class

---
+++ test/net/whitewolfdoge/skelesmite/SkSmTest.java
@@ -0,0 +1,10 @@
+package net.whitewolfdoge.skelesmite;
+
+public class SkSmTest{
+	public static void main(String[] args){
+		int x = 2, y = 4, z = 6;
+		String wldName = "testWorld";
+		String msg = String.format("Purged Skeleton Horse at (%d, %d, %d) in %s!", x, y, z, wldName);
+		System.out.println(msg);
+	}
+}
